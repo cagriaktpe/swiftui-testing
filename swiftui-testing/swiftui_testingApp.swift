237836100e551a878a6c6e2c954659eadf7cd24f
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-11-25 16:26:39 +0300

    Initial Commit

---
+++ swiftui-testing/swiftui_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_testingApp.swift
+//  swiftui-testing
+//
+//  Created by Samet Çağrı Aktepe on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
